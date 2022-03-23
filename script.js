5c2dfdf0bc596e95a03c799baa831ee8d554ab4b
verloxs <gtsbyxx@gmail.com>
2022-03-23 12:25:12 -0400

    Implemented round function.

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ let items = ["Rock", "Paper", "Scissors"];
 
 let computerPlay = () => items[Math.floor(Math.random() * items.length)];
 
-let round = (playerSelection, computerSelection) =>{
-    let playerSelection = p
-    let computerSelection = c
+let playRound = (playerSelection, computerSelection) => {
+  let p = playerSelection.toLowerCase();
+  let c = computerSelection.toLowerCase();
 
-    if (p === "Rock" && c === ) {
-        return "You Win! " + p + " beats" + v
-    }
-}
+  if (p === "rock" && c === "scissors") {
+    return "You Win! Rock beats Scissors!";
+  } else if (p === "paper" && c === "rock") {
+    return "You win! Paper beats Scissors!";
+  } else if (p === "scissors" && c === "paper") {
+    return "You win! Scissors beats Paper!";
+  } else
+    return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+};
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));

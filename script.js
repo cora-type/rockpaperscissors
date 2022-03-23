38d44a8585caf227c269695663c1f7f6bb7f962c
verloxs <gtsbyxx@gmail.com>
2022-03-23 12:04:07 -0400

    round implementation

---
+++ script.js
@@ -0,0 +1,12 @@
+let items = ["Rock", "Paper", "Scissors"];
+
+let computerPlay = () => items[Math.floor(Math.random() * items.length)];
+
+let round = (playerSelection, computerSelection) =>{
+    let playerSelection = p
+    let computerSelection = c
+
+    if (p === "Rock" && c === ) {
+        return "You Win! " + p + " beats" + v
+    }
+}
